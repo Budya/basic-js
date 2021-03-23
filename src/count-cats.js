b24418e3213754c99a97ac380f66305a6e84a4db
Dima <budya2012@gmail.com>
2021-03-23 17:07:46 +0300

    feat: implement transform array function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,22 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(matrix) {
-  let concatMatrix;
+module.exports = function countCats(matrix) {  
   let sumCats = 0;
   if (matrix === undefined || matrix.length === 0) {
     return 0;
   } else {
-    for (let i = 0; i < matrix.length; i++) {
-      if (i === 0) {
-        concatMatrix = matrix[i];
-      } else {
-        concatMatrix = concatMatrix.concat(matrix[i]);
-      }
-    }
-
-    for (let i = 0; i < concatMatrix.length; i++) {
-      if (concatMatrix[i] === "^^") {
-        sumCats += 1;
+    for(let i = 0; i < matrix.length; i++){
+      for(let j = 0; j < matrix[i].length; j++){
+        if (matrix[i][j] === "^^") {
+          sumCats += 1;
+        }
       }
     }
   }
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,54 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  let prepareArr = [];
+  let resultArr = [];
+
+  if(!Array.isArray(arr)){
+    throw new Error('Error: wrong parameter')
+  }
+  //for discardDiscarded & doubleDiscarded
+  let checkThreeLast = function (prepareArr) {
+    if ((prepareArr[prepareArr.length - 1] === '--double-prev' &&
+        prepareArr[prepareArr.length - 3] === '--discard-next') ||
+      (prepareArr[prepareArr.length - 1] === '--discard-prev' &&
+        prepareArr[prepareArr.length - 3] === '--discard-next')) {
+      prepareArr.pop();
+      prepareArr.pop();
+      prepareArr.pop();
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    prepareArr.push(arr[i]);
+    if (prepareArr.length >= 3) {
+      checkThreeLast(prepareArr);
+    }
+  }
+
+  let checkTwoLast = function (checkArr) {
+    if (checkArr[checkArr.length - 2] === '--discard-next') {
+      checkArr.pop();
+      checkArr.pop();
+    } else if (checkArr[checkArr.length - 2] === '--double-next') {
+      checkArr[checkArr.length - 2] = checkArr[checkArr.length - 1];
+    } else if (checkArr[checkArr.length - 1] === '--discard-prev') {
+      checkArr.pop();
+      checkArr.pop();
+    } else if (checkArr[checkArr.length - 1] === '--double-prev') {
+      checkArr[checkArr.length - 1] = checkArr[checkArr.length - 2]
+    }
+  }
+
+  for (let i = 0; i < prepareArr.length; i++) {
+    if (i === 0 && (prepareArr[i] === '--discard-prev' || prepareArr[i] === '--double-prev')) {
+
+    } else if (i === prepareArr.length - 1 && (prepareArr[i] === '--discard-next' || prepareArr[i] === '--double-next')) {
+
+    } else {
+      resultArr.push(prepareArr[i]);
+      checkTwoLast(resultArr);
+    }
+  }
+  
+  return resultArr;
+}
