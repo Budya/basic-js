3e99a8027a5afd7ffdc56b404ef66f7d7e9e2144
Dima <budya2012@gmail.com>
2021-03-26 15:30:35 +0300

    feat: add all functionality for crypt

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,66 @@
 const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(boolParam) {
+    this.revers = false;
+    if (typeof boolParam === 'boolean') {
+      if (boolParam.toString() === 'false') {
+        this.revers = true;
+      }
+    }
+    this.abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
-}
-
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error();
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let engStep = 0;
+    let engMessage = message.split('').filter((char) => {
+      return this.abc.indexOf(char) >= 0;
+    }).join('');
+    let result = '';
+    for (let i = 0; i < message.length; i++) { // try message length
+      if (this.abc.indexOf(message[i]) >= 0) {
+        let messageCharIndex = 
+            this.abc.indexOf(engMessage[((engStep >= engMessage.length) ? engStep % engMessage.length : engStep)]);
+        let keyChar = key[((engStep >= key.length) ? engStep % key.length : engStep)];
+        let keyCharIndex = this.abc.indexOf(keyChar);
+        let char = this.abc[(((this.abc.length + (messageCharIndex + keyCharIndex)) % this.abc.length))];
+        result += char;
+        engStep++
+      } else {
+        result += message[i];
+      }
+    };
+    if (this.revers) result = result.split('').reverse().join('');
+    return result;
+  }
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error();
+    };
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let engStep = 0;
+    let engMessage = message.split('').filter((char) => {
+      return this.abc.indexOf(char) >= 0;
+    }).join('');
+    let result = '';
+    for (let i = 0; i < message.length; i++) {
+      if (this.abc.indexOf(message[i]) >= 0) {
+        let messageCharIndex = this.abc.indexOf(engMessage[((engStep >= engMessage.length) ? engStep % engMessage.length : engStep)]);
+        let keyChar = key[((engStep >= key.length) ? engStep % key.length : engStep)];
+        let keyCharIndex = this.abc.indexOf(keyChar);
+        let char = this.abc[(((this.abc.length + (messageCharIndex - keyCharIndex)) % this.abc.length))];
+        result += char;
+        engStep++
+      } else {
+        result += message[i];
+      }
+    };
+    if (this.revers) result = result.split('').reverse().join('');
+    return result;
+  }
+};
 module.exports = VigenereCipheringMachine;
